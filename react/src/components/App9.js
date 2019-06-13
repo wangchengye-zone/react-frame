f1e856f1dc67d56eb350f29a4d2a4ccc619123cf
zazazazha <18895361867@163.com>
2019-06-13 20:09:46 +0800

    first commit

---
+++ react/src/components/App9.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+constructor (props) {
+  super(props);
+  console.log('1',this);
+  this.btnClickFun = this.btnClickFn.bind(this)
+}
+btnClickFn () {
+  console.log('2',this);
+}
+
+  // render () {
+  //   return (
+  //     <div>
+  //       <button onClick = { this.btnClickFun}>react按钮</button>
+  //     </div>
+  //   )
+  // }
+  render () {
+    return (
+      <div>
+        <button onClick = { this.btnClickFn.bind(this)}>react按钮</button>
+      </div>
+    )
+  }
+}
+
+export default App
